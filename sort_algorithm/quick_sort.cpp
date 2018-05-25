edea5fb6b90468179f919327f66ed007e26f66fd
燕武 <38755286+yanwu9887@users.noreply.github.com>
2018-05-25 20:37:21 +0800

    add test

--- sort_algorithm/quick_sort.cpp
+++ sort_algorithm/quick_sort.cpp
@@ -1,3 +1,23 @@
+#include<iostream>
+using namespace std;
+void print_array(int a[],int length);
+void quick_sort(int a[], int start, int end);
+int partition(int a[], int start, int end);
+
+int main() {
+	int a[5] = {2,1 ,9 ,7,0};
+	quick_sort(a,0,4);
+	print_array(a,5);
+	system("pause");
+	return 0;
+}
+
+void print_array(int a[],int length) {
+	for (int i = 0;i < length;i++) {
+		cout << a[i] << endl;
+	}
+}
+
 void quick_sort(int a[], int start, int end) {
 	if (start >= end) return;
 	int mid = partition(a, start, end);
