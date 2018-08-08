beec75de460b6b4c35e5c53ab0f0cde93270875c
燕武 <38755286+yanwu9887@users.noreply.github.com>
2018-08-08 15:36:52 +0800

    Update MOG_opencv.cpp

--- MOG_opencv.cpp
+++ MOG_opencv.cpp
@@ -1,46 +1,32 @@
-#include "stdafx.h"
-#include <cv.h>
-#include <highgui.h>
-#include <math.h>
-#include <time.h>
-#include <iostream>
-#include <fstream>
-#include <Windows.h>  
-#include <opencv2/opencv.hpp> 
 
-using namespace cv;  
+#include "opencv2/opencv.hpp"
+#include "opencv2/video/background_segm.hpp"
+ 
+using namespace cv;
 using namespace std;
-
-bool pause = false;//是否暂停
-int frameNum = 0;
-
 int main()
 {
-    uchar key = false;//用来设置暂停
-
-    VideoCapture video("test2.avi"); 
-    //Mat frame,mask,thresholdImage,output;
-    Mat frame,mask;
-    video>>frame;
-    //bgSubtractor(history,nmixtures,backgroundRatio,noiseSigma)  
-    BackgroundSubtractorMOG bgSubtractor(5,2,0.75,false);
-
-    while(true){
-        video>>frame;
-        ++frameNum;
-        //bgSubtractor(当前帧，二值图，学习率);
-        bgSubtractor(frame,mask,0.01);
-        imshow("frame",frame);
-        imshow("mask",mask);
-        waitKey(20);
-
-        //按键P切换暂停和播放
-        key = cvWaitKey(1);
-        if(key == 'p') pause = true;
-        while(pause)
-            if(cvWaitKey(0)=='p')
-                pause = false;  
-    }
-
-    return 0;
+	VideoCapture video("../video.avi");
+	int frameNum = 1;
+	Mat frame, mask, thresholdImage, output;
+	if (!video.isOpened())
+		cout << "fail to open!" << endl;
+	//cout<<video.isOpened();
+	double totalFrameNumber = video.get(CAP_PROP_FRAME_COUNT);
+	video >> frame;
+	Ptr<BackgroundSubtractorMOG2> bgsubtractor = createBackgroundSubtractorMOG2();
+	bgsubtractor->setVarThreshold(20);
+	
+	while (true){
+		if (totalFrameNumber == frameNum)
+			break;
+		video >> frame;
+		++frameNum;
+		//bgSubtractor(frame, mask, 0.001);
+		bgsubtractor->apply(frame, mask, 0.01);
+		imshow("mask", mask);
+		waitKey(10);
+	}
+	
+	return 0;
 }
